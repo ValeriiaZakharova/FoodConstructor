8b4a69206aed95195882bd7484a83a2e5c956155
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-02-25 09:57:37 +0200

    Create project

---
+++ FoodConstructor/FoodConstructor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodConstructor
+//
+//  Created by Valeriia Zakharova on 25.02.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
