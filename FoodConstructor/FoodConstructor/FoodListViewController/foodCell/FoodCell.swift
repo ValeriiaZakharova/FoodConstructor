956456d1c045f0492f9eb3c3bf4ca934b65d794f
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-02-26 14:06:33 +0200

    added table view

---
+++ FoodConstructor/FoodConstructor/FoodListViewController/foodCell/FoodCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodCell.swift
+//  FoodConstructor
+//
+//  Created by Valeriia Zakharova on 26.02.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+
+class FoodCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
