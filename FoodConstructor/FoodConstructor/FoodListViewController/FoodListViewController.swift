3d9000edbc457fb4541ec0603c4be3a511769177
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-02-26 13:56:02 +0200

    added foodlistviewcontroller

---
+++ FoodConstructor/FoodConstructor/FoodListViewController/FoodListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FoodListViewController.swift
+//  FoodConstructor
+//
+//  Created by Valeriia Zakharova on 26.02.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+
+class FoodListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    
+    
+
+}
